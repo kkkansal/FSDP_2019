8757098a386de9d1277886260aa2d956108d7ea8
kkkansal <kunalkansal99@gmail.com>
2019-06-18 10:43:53 -0700

    spaceseperateddata_matplotlib

---
+++ spaceseperateddata.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 03:27:48 2019
+
+@author: kunal
+"""
+
+import numpy as np 
+x=[6, 9, 2, 3, 5, 8, 1, 5, 4]
+x = np.array(x)
+print(x)
+x = x.reshape(3,3)
+print (x)
