d6d7c58557556af0c19ae1a99d97a58700c2c8e1
kkkansal <kunalkansal99@gmail.com>
2020-01-05 17:54:29 +0530

    even_odd

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 17:25:24 2020
+
+@author: kunal
+"""
+
+ #to find out even and odd
+kunal=int(input("enter the number"))
+if(kunal%2==0):
+    print("even")
+else:
+    print("odd")
